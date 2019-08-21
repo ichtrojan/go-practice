94869db374f073b0ecdebe7c24c756bc4d4e7332
Michael okoh <ordrizzy@gmail.com>
2019-08-21 17:03:33 +0100

    switch

---
+++ switch.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main () {
+  // Declare the desire
+  desire := "money"
+
+  switch (desire) {
+    case "love":
+      fmt.Println("❤️❤️❤️❤️❤️❤️❤️")
+
+    case "money":
+      fmt.Println("💰💰💰💰💰💰💰")
+
+    default:
+      fmt.Println("I cannot give you what you desire")
+  }
+}
