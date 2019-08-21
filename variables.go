23f0df034f0c7ed6682c0477420e9b9a0e5185eb
Michael okoh <ordrizzy@gmail.com>
2019-08-21 17:03:25 +0100

    variables

---
+++ variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main () {
+  name := "Trojan Okoh"
+  age := 100
+  height := 5.85
+
+  fmt.Println("My name is ", name, "\nI am ", age, " years old and", height, " feets tall")
+}
