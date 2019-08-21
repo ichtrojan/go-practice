319944cdc73d8cc8dbf74283c8f01ca42a8bdb39
Michael okoh <ordrizzy@gmail.com>
2019-08-21 17:03:19 +0100

    arrays

---
+++ array.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main () {
+  // Declare an array to hold 5 integers
+  numbers := [5]int{1, 2, 3, 4, 5}
+
+  // Print a single element
+  fmt.Println(numbers[3])
+
+  // Loop through the array to print every value
+  for _, number := range numbers {
+    fmt.Println(number)
+  }
+}
