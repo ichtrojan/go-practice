7ecb36f554178e59592c9cdb893df9de74f12857
Michael okoh <ordrizzy@gmail.com>
2019-08-21 17:03:52 +0100

    booleans and conditions

---
+++ boolean-and-condition.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main () {
+  single := true
+
+  if (single) {
+    fmt.Println("Trojan is Single")
+  } else {
+    fmt.Println("Trojan is not single")
+  }
+
+  employed := false
+
+  if (employed) {
+    fmt.Println("Kofo is Employed")
+  } else {
+    fmt.Println("Kofo is Unemployed and Jobless at the same time")
+  }
+}
