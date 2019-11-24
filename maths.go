de5028b09268b60a3de2caaa7bd0b91902a71a55
Bambo <bambo@prayerbox.co>
2019-11-24 10:00:35 +0100

    Math methods

--- maths.go
+++ maths.go
@@ -5,12 +5,24 @@ import (
 	"math"
 )
 
-// find mix or max number between two defined numbers
-
 func main() {
+	mathmethods()
+}
+
+func mathmethods() {
 	a := math.Min(100, 20)
-	fmt.Println(a)
+	fmt.Println("Minimum number between 100 and 20 is", a)
 
 	b := math.Max(100, 20)
-	fmt.Println(b)
+	fmt.Println("Maximum number between 100 and 20 is", b)
+
+	fmt.Println("The square root of 16 is", math.Sqrt(16)) //square root
+	fmt.Println("1 + 2 = ", 1+2)                           // Addition
+	fmt.Println("5 * 2 = ", 5*2)                           // Multiplication
+	fmt.Println("10 - 2 = ", 10-2)                         // Subtraction
+	fmt.Println("10 / 2 = ", 10/2)                         // Division
+	fmt.Println("10 % 2 = ", 10%2)                         // Remainder
+
+	u := (10 + 10) * 5
+	fmt.Println("10 + 10 * 5 = ", u)
 }
